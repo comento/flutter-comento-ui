92760999716913c0f04215636fdf88944fc2623f
Heewookji <jhu2269@gmail.com>
2021-08-13 01:33:01 +0900

    fix: cds app bar

--- comento_design_system/lib/src/components/app_bars/cds_app_bar.dart
+++ comento_design_system/lib/src/components/app_bars/cds_app_bar.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
 import '../../../comento_design_system.dart';
+import '../../strings.dart';
 
 class CdsAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final double _leadingWidth = 40;
   final Widget? leading;
   final Widget? title;
   final List<Widget>? actions;
@@ -18,16 +20,63 @@ class CdsAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      leading: leading,
+      leading: leading ?? _getAppBarLeading(context),
+      leadingWidth: _leadingWidth,
       title: title,
-      actions: actions,
       centerTitle: centerTitle,
+      actions: actions,
       backgroundColor: CdsColors.white,
-      elevation: 4,
-      shadowColor: CdsColors.grey100,
+      elevation: 0,
+      titleSpacing: 8,
+      bottom: _buildBottomBorder(),
+    );
+  }
+
+  PreferredSize _buildBottomBorder() {
+    return PreferredSize(
+      child: Container(
+        color: CdsColors.grey100,
+        height: 1.0,
+      ),
+      preferredSize: Size.fromHeight(1.0),
     );
   }
 
   @override
-  Size get preferredSize => Size.fromHeight(48);
+  Size get preferredSize => Size.fromHeight(49);
+
+  Widget? _getAppBarLeading(BuildContext context) {
+    final ModalRoute<Object?>? parentRoute = ModalRoute.of(context);
+    final bool canPop = parentRoute?.canPop ?? false;
+    Widget? leading = canPop ? CdsBackButton() : null;
+    if (leading != null) {
+      leading = new ConstrainedBox(
+        constraints: BoxConstraints.tightFor(width: _leadingWidth),
+        child: leading,
+      );
+    }
+
+    return leading;
+  }
+}
+
+class CdsBackButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(left: 16),
+      child: IconButton(
+        icon: Image.asset(
+          'assets/images/icons/backward.png',
+          package: packageName,
+        ),
+        tooltip: MaterialLocalizations.of(context).backButtonTooltip,
+        onPressed: () {
+          Navigator.of(context).maybePop();
+        },
+        padding: EdgeInsets.zero,
+        constraints: BoxConstraints(),
+      ),
+    );
+  }
 }
