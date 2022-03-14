3467e2a7f7d272c6b57b76c37bf682763b2e4f43
Heewookji <jhu2269@gmail.com>
2022-03-14 17:01:58 +0900

    fix: color

--- comento_design_system/lib/src/styles/cds_colors.dart
+++ comento_design_system/lib/src/styles/cds_colors.dart
@@ -9,49 +9,62 @@ class CdsColors {
   static const Color google = const Color(0xFFDC4E41);
   static const Color facebook = const Color(0xFF1877F2);
   static const Color apple = const Color(0xFF000000);
-  static const Color comento = green600;
+  static const Color comento = primary;
 
 // context color
+  static const Color primary = blue600;
+  static const Color lightPrimary = blue100;
+  static const Color secondary = orange600;
+  static const Color success = green600;
   static const Color error = red600;
+  static const Color info = grey600;
+  static const Color yellow = yellow600;
 
 // color
   static const Color black = const Color(0xFF000000);
   static const Color white = const Color(0xFFFFFFFF);
 
-  static const Color grey900 = const Color(0xFF151617);
-  static const Color grey850 = const Color(0xFF21282F);
-  static const Color grey800 = const Color(0xFF495057);
-  static const Color grey700 = const Color(0xFF6F767D);
-  static const Color grey600 = const Color(0xFF838990);
-  static const Color grey500 = const Color(0xFF979CA2);
-  static const Color grey400 = const Color(0xFFBEC2C6);
-  static const Color grey300 = const Color(0xFFD2D5D8);
-  static const Color grey250 = const Color(0xFFDEE0E3);
-  static const Color grey200 = const Color(0xFFE8EAED);
-  static const Color grey100 = const Color(0xFFF4F5F7);
-  static const Color grey000 = const Color(0xFFFAFBFC);
+  static const Color grey900 = const Color(0xFF040505);
+  static const Color grey850 = const Color(0xFF202325);
+  static const Color grey800 = const Color(0xFF3C4144);
+  static const Color grey700 = const Color(0xFF595F63);
+  static const Color grey600 = const Color(0xFF767D82);
+  static const Color grey500 = const Color(0xFF949BA0);
+  static const Color grey400 = const Color(0xFFADB3B8);
+  static const Color grey300 = const Color(0xFFCFD4D7);
+  static const Color grey250 = const Color(0xFFE1E4E6);
+  static const Color grey200 = const Color(0xFFEAECEE);
+  static const Color grey100 = const Color(0xFFF3F4F5);
+  static const Color grey000 = const Color(0xFFFCFCFC);
 
-  static const Color green800 = const Color(0xFF00B334);
-  static const Color green600 = const Color(0xFF00C854);
-  static const Color green400 = const Color(0xFF5CE596);
-  static const Color green100 = const Color(0xFFDEFAEB);
-  static const Color green000 = const Color(0xFFF7FCF9);
+  static const Color green800 = const Color(0xFF038542);
+  static const Color green600 = const Color(0xFF04CD66);
+  static const Color green400 = const Color(0xFF54DD97);
+  static const Color green100 = const Color(0xFFCDF5E0);
+  static const Color green000 = const Color(0xFFF5FDF9);
 
-  static const Color red800 = const Color(0xFFCC2727);
-  static const Color red600 = const Color(0xFFFA5252);
-  static const Color red400 = const Color(0xFFFF8888);
-  static const Color red100 = const Color(0xFFFFDFDF);
-  static const Color red000 = const Color(0xFFFFF7F7);
+  static const Color red800 = const Color(0xFF971818);
+  static const Color red600 = const Color(0xFFE92525);
+  static const Color red400 = const Color(0xFFF06B6B);
+  static const Color red100 = const Color(0xFFFBD3D3);
+  static const Color red000 = const Color(0xFFFEF6F6);
 
-  static const Color blue800 = const Color(0xFF0074CC);
-  static const Color blue600 = const Color(0xFF1E90FF);
-  static const Color blue400 = const Color(0xFF7EC1FF);
-  static const Color blue100 = const Color(0xFFD6EBFF);
-  static const Color blue000 = const Color(0xFFF2FAFF);
+  static const Color orange800 = const Color(0xFFCF5310);
+  static const Color orange600 = const Color(0xFFEE7E41);
+  static const Color orange400 = const Color(0xFFF3A77E);
+  static const Color orange100 = const Color(0xFFFCE5D9);
+  static const Color orange000 = const Color(0xFFFEFAF7);
 
-  static const Color yellow600 = const Color(0xFFFFDD33);
-  static const Color yellow400 = const Color(0xFFFFEE7A);
-  static const Color yellow100 = const Color(0xFFFFFACC);
+  static const Color yellow600 = const Color(0xFFFFD700);
+  static const Color yellow400 = const Color(0xFFFFE452);
+  static const Color yellow100 = const Color(0xFFFFF7CC);
+  static const Color yellow000 = const Color(0xFFFFFDF5);
+
+  static const Color blue800 = const Color(0xFF1B5192);
+  static const Color blue600 = const Color(0xFF2A7DE1);
+  static const Color blue400 = const Color(0xFF6EA7EB);
+  static const Color blue100 = const Color(0xFFD4E5F9);
+  static const Color blue000 = const Color(0xFFF7FAFE);
 
   static const Color transparent = const Color(0x00000000);
 }
