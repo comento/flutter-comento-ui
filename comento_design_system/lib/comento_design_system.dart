727e8ec7b0c3dbfaec7dd95f042ae16c874bff5b
heewookji <jhu2269@gmail.com>
2023-10-17 10:03:47 +0900

    feat: textStyle extension 추가

--- comento_design_system/lib/comento_design_system.dart
+++ comento_design_system/lib/comento_design_system.dart
@@ -1,8 +1,9 @@
 library comento_design_system;
 
 export 'src/components.dart';
-export 'src/styles.dart';
 export 'src/enums.dart';
+export 'src/extensions.dart';
+export 'src/styles.dart';
 
 // Helpers
 // export 'src/shared/ui_helpers.dart';
