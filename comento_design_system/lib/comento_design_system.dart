b6306a4b5405d7a51180f56163f5f67fe2e4268f
heewookji <jhu2269@gmail.com>
2023-10-17 10:42:46 +0900

    fix: extension 제거

--- comento_design_system/lib/comento_design_system.dart
+++ comento_design_system/lib/comento_design_system.dart
@@ -2,7 +2,6 @@ library comento_design_system;
 
 export 'src/components.dart';
 export 'src/enums.dart';
-export 'src/extensions.dart';
 export 'src/styles.dart';
 
 // Helpers
--- comento_design_system/lib/src/extensions/text_style_extension.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-extension ExtendsTextStyle on TextStyle {
-  TextStyle withColor(Color color) {
-    return copyWith(color: color);
-  }
-}
